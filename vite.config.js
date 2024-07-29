a184776d6c1bd27e1408431b50fb581d3ea4921d
jems5 <chanjeremy5@gmail.com>
2024-07-30 01:11:16 +0800

    removed react-entypo

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ["react-entypo"], // Only if you want to explicitly externalize it
-    },
+    rollupOptions: {},
   },
 });
